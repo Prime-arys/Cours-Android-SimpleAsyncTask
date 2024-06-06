e6c28d9ae58cb84488b96be947e3c1dccd0e520b
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2024-06-06 16:13:07 +0200

    init

---
+++ app/src/main/java/esgi/chaussy/simpleasynctask/MainActivity.java
@@ -0,0 +1,52 @@
+package esgi.chaussy.simpleasynctask;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TEXT_STATE = "currentText";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mTextView = (TextView) findViewById(R.id.textView1);
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+
+
+    }
+
+    private TextView mTextView;
+
+
+    public void startTask(View view) {
+        // Placer un message dans la vue texte
+        mTextView.setText(R.string.napping);
+        // Démarre l'AsyncTask.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Enregistre l'état de TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
